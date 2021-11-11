212ee33425accd60cd1509113b491f4dbb289d33
rambkk <37767945+rambkk@users.noreply.github.com>
2021-11-11 15:14:37 +0700

    changed the default value of $i to match Javascript reduce method

--- array_reduce_JS.php
+++ array_reduce_JS.php
@@ -1,6 +1,6 @@
 <?php
 /*
- * PHP array_reduce_JS (v0.1) - array_reduce with additional parameters
+ * PHP array_reduce_JS (v0.11) - array_reduce with additional parameters
  * (javascript like reduce method)
  *
  * array_reduce with key
@@ -18,21 +18,25 @@
  * ---------------
  * array_reduce_JS ( ARRAY, CALLBACK($c,$v,$i,$a), [optional: initial value of $c] )
  * ---------------
- * This works the same way as general PHP array_reduce function with
- * the addition of additional optional parameters to the CALLBACK
- *
- * Run CALLBACK on each item of ARRAY and put the result as $c for the next CALLBACK
+ * This works the SIMILAR way as general PHP array_reduce function with
+ * the addition of additional optional parameters to the CALLBACK.
+ * The difference is the initial default value of $i, in PHP array_reduce, the default 
+ * value of $i is null while in Javascript reduce method, if $i is not specified, 
+ * the default is the first item of the ARRAY and processing starts with the second 
+ * value of the ARRAY.
+ * 
+ * General explanation:
+ * Run CALLBACK on each items of ARRAY and put the result as $c for the next CALLBACK
  * Result from CALLBACK on last item will be returned
- * The default initial value of $c is null
  *
- * NOTE: This can work as a drop-in replacement for array_reduce function
- * NOTE: For more information look up PHP array_reduce function
+ * NOTE: This might not work properly as a drop-in replacement for array_reduce function
+ * NOTE: For more information look up Javascript reduce method and PHP array_reduce function
  * NOTE: array_reduce_JS needs more processing resources than the built-in array_reduce
  *
  * -------------------
  * CALLBACK parameters
  * -------------------
- * $c (ONLY for array_reduce_JS) initial value or return value from the last CALLBACK
+ * $c initial value or return value from the last CALLBACK
  * $v value of current item
  * $i index of current item (optional)
  * $a current array (optional)
@@ -45,7 +49,7 @@
  * Arrow function (PHP 7 >= 7.4, PHP 8) (optional)
  *
  *
- * Author: Ram Narula <github: rambkk> OR <www.pluslab.net>
+ * Author: Ram Narula <github rambkk> OR <pluslab.net>
  *
  *
  * How to use? 
@@ -62,7 +66,8 @@
 /* ----------- */
 
 
-function array_reduce_JS($a,$f,$c=null){foreach($a as $k=>$v)$c=$f($c,$v,$k,$a);return $c;}
+function array_reduce_JS($a,$f,...$arg){ $arg?$c=$arg[0]:$c=$a[key($a)];foreach($arg?$a:array_slice($a,1,1,true) as $k=>$v)$c=$f($c,$v,$k,$a);return $c; } 
+
 
 //Anonymous functions as some might prefer
-//$array_reduce_JS=function($a,$f,$c=null){foreach($a as $k=>$v)$c=$f($c,$v,$k,$a);return $c;};
+//$array_reduce_JS=function($a,$f,...$arg){ $arg?$c=$arg[0]:$c=$a[key($a)];foreach($arg?$a:array_slice($a,1,1,true) as $k=>$v)$c=$f($c,$v,$k,$a);return $c; };
